210c04016775a35571e1aa55c8fc09c93d172d90
ETdoFresh <ETdoFresh@gmail.com>
2019-09-23 10:34:47 -0500

    Update entrypoint.sh

--- UnityBuild/entrypoint.sh
+++ UnityBuild/entrypoint.sh
@@ -1,7 +1,17 @@
 #!/bin/sh -l
 
+echo Inputs
+echo ----------------------
 echo UNITY_USERNAME: $1
 echo UNITY_PASSWORD: $2
 echo BUILD_NAME: $3
 echo BUILD_TARGET: $4
 echo DROPBOX_TOKEN: $5
+
+echo Environment Variables
+echo ----------------------
+echo BUILD_PATH: $BUILD_PATH
+echo TEST_PLATFORM: $TEST_PLATFORM
+echo WORKDIR: $WORKDIR
+echo UNITY_ACTIVATION_FILE: $UNITY_ACTIVATION_FILE
+echo UNITY_LICENSE_FILE: $UNITY_LICENSE_FILE
