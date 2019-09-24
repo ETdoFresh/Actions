7bf3c5a08af58302d1aa6a89bb36f82f5d45d522
ETdoFresh <ETdoFresh@gmail.com>
2019-09-24 07:03:20 -0500

    Update UnityBuild

--- UnityBuild/entrypoint.sh
+++ UnityBuild/entrypoint.sh
@@ -8,6 +8,7 @@ echo BUILD_NAME: $3
 echo BUILD_TARGET: $4
 echo DROPBOX_TOKEN: $5
 echo
+
 echo Environment Variables
 echo ----------------------
 echo BUILD_PATH: $BUILD_PATH
@@ -16,15 +17,20 @@ echo WORKDIR: $WORKDIR
 echo UNITY_ACTIVATION_FILE: $UNITY_ACTIVATION_FILE
 echo UNITY_LICENSE_FILE: $UNITY_LICENSE_FILE
 echo
+
 echo pwd
 pwd
 echo
+
 echo ls -l
 ls -l
 echo
+
 echo rm -Rf $BUILD_PATH || true
 rm -Rf $BUILD_PATH || true
 echo mkdir -p $BUILD_PATH
+
 mkdir -p $BUILD_PATH
 echo ls -l
 ls -l
+
