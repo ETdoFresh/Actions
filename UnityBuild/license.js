a78ce5e4781109cf744f86bd7d5da8db665ca3fc
ETdoFresh <ETdoFresh@gmail.com>
2019-09-25 11:24:30 -0500

    Update license.js

--- UnityBuild/license.js
+++ UnityBuild/license.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 (async () => {
 	
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({args: ['--no-sandbox']});
     const page = await browser.newPage();
 
     // open manual page
